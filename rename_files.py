edbe26adb3b67c2c3e69cc94f915164057c30921
Bryan Quille <bryanquille@outlook.com>
2023-02-12 02:01:07 -0500

    Adding an image

--- rename_files.py
+++ rename_files.py
@@ -1,5 +1,7 @@
+# Libraries
 import os
 from tkinter import *
+
 
 # Color variables
 grayColor = "#cacaca"
@@ -10,32 +12,41 @@ blackColor = "#343434"
 # Window
 win1 = Tk()
 win1.title("Rename Files")
-win1.geometry("900x400")
+win1.geometry("1100x500")
 win1.resizable(width=False, 
                height=False)
 win1.config(bg=whiteColor)
 
+# Image
+img = PhotoImage(file="rename_files.png")
+
 # Labels
-lab1 = Label(win1, 
-             width=20, 
-             text="Directory", 
-             bg=grayColor, 
-             fg=blackColor, 
+lab0 = Label(win1,
+             width=20,
+             text="Rename Files\n Program",
+             bg=grayColor,
+             fg=blackColor,
              font="Ubuntu")
-lab1.grid(row=1, 
+lab0.grid(row=0, 
           column=1, 
           ipadx=5, 
           ipady=5, 
           padx=20, 
           pady=10)
 
+lab1 = Label(win1, 
+             image=img,
+             bg=whiteColor)
+lab1.grid(row=0, 
+         column=2)
+
 lab2 = Label(win1, 
              width=20, 
-             text="New name for files", 
+             text="Directory", 
              bg=grayColor, 
              fg=blackColor, 
              font="Ubuntu")
-lab2.grid(row=2, 
+lab2.grid(row=1, 
           column=1, 
           ipadx=5, 
           ipady=5, 
@@ -44,11 +55,11 @@ lab2.grid(row=2,
 
 lab3 = Label(win1, 
              width=20, 
-             text="Give the file extension\n (e.g. .txt, .jpg)", 
+             text="New name for files", 
              bg=grayColor, 
              fg=blackColor, 
              font="Ubuntu")
-lab3.grid(row=3, 
+lab3.grid(row=2, 
           column=1, 
           ipadx=5, 
           ipady=5, 
@@ -56,11 +67,24 @@ lab3.grid(row=3,
           pady=10)
 
 lab4 = Label(win1, 
-             width=60, 
+             width=20, 
+             text="Give the file extension\n (e.g. .txt, .jpg)", 
              bg=grayColor, 
              fg=blackColor, 
              font="Ubuntu")
-lab4.grid(row=4, 
+lab4.grid(row=3, 
+          column=1, 
+          ipadx=5, 
+          ipady=5, 
+          padx=20, 
+          pady=10)
+
+lab5 = Label(win1, 
+             width=55, 
+             bg=grayColor, 
+             fg=blackColor, 
+             font="Ubuntu")
+lab5.grid(row=4, 
           column=2, 
           ipadx=5, 
           ipady=5, 
@@ -120,7 +144,7 @@ def renameFiles():
         os.rename(lastFile, newFile)
         sequence += 1
 
-    lab4.configure(text="The files were rename correctly.")
+    lab5.configure(text="The files were rename correctly.")
     
 # Button
 btn = Button(win1, 
